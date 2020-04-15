efe68802dacae0498431acf3310a74ca922a84bf
naimrodriguez984@gmail.com <naimrodriguez984@gmail.com>
2020-04-15 17:15:48 +0300

    segundo

--- src/Calculadora/CalculadoraTest.java
+++ src/Calculadora/CalculadoraTest.java
@@ -10,8 +10,8 @@ public class CalculadoraTest {
 		
 	@Test
 	public void testSuma() {
-		int resultado = Calculadora.suma(2, 3); 
-		int esperado = 5;
+		int resultado = Calculadora.suma(5, 6); 
+		int esperado = 11;
 		assertEquals(esperado, resultado);
 		
 	}
